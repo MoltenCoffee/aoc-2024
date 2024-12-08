0522c5d127c965f1218440ab5c613619f9b57dc9
MoltenCoffee <13321277+MoltenCoffee@users.noreply.github.com>
2024-12-08 16:23:41 +0100

    Day 3 part 2

--- src/day3.ts
+++ src/day3.ts
@@ -2,13 +2,42 @@ import { getInputLines, printPart } from "./util";
 
 const memory = await getInputLines(3);
 
-const muls = memory.flatMap((line) => {
-  return Array.from(line.matchAll(/mul\((\d+,\d+)\)/g), (m) => m[1]).map(
-    (pair) => {
-      const [a, b] = pair.split(",");
-      return parseInt(a) * parseInt(b);
-    }
-  );
-});
+const muls = memory.flatMap((line) =>
+  Array.from(line.matchAll(/mul\((\d+,\d+)\)/g), (m) => m[1]).map((pair) => {
+    const [a, b] = pair.split(",");
+    return parseInt(a) * parseInt(b);
+  })
+);
 
-printPart(1, muls.reduce((acc, val) => acc + val, 0));
+printPart(
+  1,
+  muls.reduce((acc, val) => acc + val, 0)
+);
+
+let enabled = true;
+printPart(
+  2,
+  memory
+    .flatMap((line) =>
+      Array.from(
+        line.matchAll(/(mul\((\d+,\d+)\))|(don't\(\))|(do\(\))/g),
+        (m) => m[2] || m[0]
+      )
+    )
+    .map((op) => {
+      switch (op) {
+        case "do()":
+          enabled = true;
+          break;
+        case "don't()":
+          enabled = false;
+          break;
+        default:
+          if (enabled) {
+            const [a, b] = op.split(",");
+            return parseInt(a) * parseInt(b);
+          }
+      }
+    })
+    .reduce((acc, val) => acc + (val || 0), 0)
+);
