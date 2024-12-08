ce7c0f07f72d01094bceb92a41e302994631d1df
MoltenCoffee <13321277+MoltenCoffee@users.noreply.github.com>
2024-12-08 15:55:43 +0100

    Finish day 2

--- src/day2.ts
+++ src/day2.ts
@@ -1,23 +1,27 @@
 import { getInputLines, printPart } from "./util";
 
-const lines = await getInputLines(2);
+const areLevelsSafe = (numbers: number[]) =>
+  (numbers.every((num, i) => (i === 0 ? true : num >= numbers[i - 1])) ||
+    numbers.every((num, i) => (i === 0 ? true : num <= numbers[i - 1]))) &&
+  numbers.every((num, i) =>
+    i === 0
+      ? true
+      : Math.abs(num - numbers[i - 1]) <= 3 && num !== numbers[i - 1]
+  );
 
-const safeReportCount = lines.reduce((acc, line) => {
-  const numbers = line.split(/\s+/).map((num) => parseInt(num));
-
-  if (
-    (numbers.every((num, i) => (i === 0 ? true : num >= numbers[i - 1])) ||
-      numbers.every((num, i) => (i === 0 ? true : num <= numbers[i - 1]))) &&
-    numbers.every((num, i) =>
-      i === 0
-        ? true
-        : Math.abs(num - numbers[i - 1]) <= 3 && num !== numbers[i - 1]
+const [partOne, partTwo] = await getInputLines(2).then((lines) =>
+  lines
+    .map((line) => line.split(/\s+/).map((num) => parseInt(num)))
+    .reduce(
+      ([partOne, partTwo], numbers) =>
+        areLevelsSafe(numbers)
+          ? [partOne + 1, partTwo + 1]
+          : numbers.some((num, i) => areLevelsSafe(numbers.toSpliced(i, 1)))
+          ? [partOne, partTwo + 1]
+          : [partOne, partTwo],
+      [0, 0]
     )
-  ) {
-    return acc + 1;
-  }
+);
 
-  return acc;
-}, 0);
-
-printPart(1, safeReportCount);
+printPart(1, partOne);
+printPart(2, partTwo);
