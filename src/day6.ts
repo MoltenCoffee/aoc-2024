3af15bc5da0f476c736bcbb95f5b3254e9a492b5
MoltenCoffee <13321277+MoltenCoffee@users.noreply.github.com>
2024-12-15 16:36:21 +0100

    Day 6 part - doesn't work

--- src/day6.ts
+++ src/day6.ts
@@ -1,8 +1,20 @@
 import { getInputLines, printPart } from "./util";
 
 const lines = await getInputLines(6);
+// const lines = `....#.....
+// .........#
+// ..........
+// ..#.......
+// .......#..
+// ..........
+// .#..^.....
+// ........#.
+// #.........
+// ......#...`.split("\n");
 const labHeight = lines.length;
 const labWidth = lines[0].length;
+
+console.log(labHeight, labWidth);
 
 const startPos = { x: 0, y: 0 };
 const guardCoords = {
@@ -21,21 +33,37 @@ for (let y = 0; y < lines.length; y++) {
   }
 }
 
-const peek = () => {
+const peek = (addedObstacleCoords?: { x: number; y: number }) => {
   const { x, y, direction } = guardCoords;
+  let nextPos = { x, y };
   switch (direction) {
     case 0:
-      return lines[y - 1]?.[x];
+      nextPos.y--;
+      break;
     case 1:
-      return lines[y][x + 1];
+      nextPos.x++;
+      break;
     case 2:
-      return lines[y + 1]?.[x];
+      nextPos.y++;
+      break;
     case 3:
-      return lines[y][x - 1];
+      nextPos.x--;
+      break;
   }
+
+  return addedObstacleCoords &&
+    addedObstacleCoords.x === nextPos.x &&
+    addedObstacleCoords.y === nextPos.y
+    ? "#"
+    : lines[nextPos.y]?.[nextPos.x];
 };
 
-const walk = () => {
+const walk = (addedObstacleCoords?: { x: number; y: number }) => {
+  if (peek(addedObstacleCoords) === "#") {
+    // Turn right
+    guardCoords.direction = (guardCoords.direction + 1) % 4;
+  }
+
   switch (guardCoords.direction) {
     case 0:
       guardCoords.y--;
@@ -52,22 +80,65 @@ const walk = () => {
   }
 };
 
-const visited = new Set<string>();
-while (
+const isInside = () =>
   guardCoords.y >= 0 &&
   guardCoords.y < labHeight &&
   guardCoords.x >= 0 &&
-  guardCoords.x < labWidth
-) {
-  const key = `${guardCoords.x},${guardCoords.y}`;
-  visited.add(key);
+  guardCoords.x < labWidth;
 
-  if (peek() === "#") {
-    // Turn right
-    guardCoords.direction = (guardCoords.direction + 1) % 4;
-  } else {
-    walk();
-  }
+const visited = new Set<string>();
+while (isInside()) {
+  visited.add(`${guardCoords.x},${guardCoords.y}`);
+  walk();
 }
 
 printPart(1, visited.size);
+
+const isLooping = (steps: Array<string>) => {
+  if (steps.length <= 3) {
+    return false;
+  }
+
+  const lastSteps = steps.slice(-2);
+
+  for (let i = 0; i < steps.length - 3; i++) {
+    if (steps[i] === lastSteps[0] && steps[i + 1] === lastSteps[1]) {
+      return true;
+    }
+  }
+};
+
+let possibleObstaclePositionCount = 0;
+for (let y = 0; y < labHeight; y++) {
+  for (let x = 0; x < labWidth; x++) {
+    if (
+      lines[y][x] === "#" || // Already an obstacle
+      lines[y][x] === "^" || // Guard starting position
+      !visited.has(`${x},${y}`) // Not visited
+    ) {
+      continue;
+    }
+    guardCoords.x = startPos.x;
+    guardCoords.y = startPos.y;
+    guardCoords.direction = 0;
+
+    const loop = new Array<string>();
+    let stepCount = 0;
+    while (isInside()) {
+      if (isLooping(loop)) {
+        possibleObstaclePositionCount++;
+        console.log(
+          `Looping with obstacle on ${x},${y} after ${stepCount} steps`
+        );
+        break;
+      }
+
+      loop.push(`${guardCoords.x},${guardCoords.y}`);
+      walk({ x, y });
+      stepCount++;
+    }
+  }
+}
+
+// Somehow this doesn't work...
+printPart(2, possibleObstaclePositionCount);
